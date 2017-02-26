06617ce6b585c75193e3e7e97e2302c847341981
elzaharieva <el.zaharieva28@gmail.com>
2017-02-26 15:47:05 +0200

    final

---
+++ KnapsackProblem/src/bg/uni_sofia/fmi/ai/KnapsackObj.java
@@ -0,0 +1,31 @@
+package bg.uni_sofia.fmi.ai;
+
+public class KnapsackObj {
+	private long weight;
+	private long cost;
+
+	public KnapsackObj(long[] arr) {
+		if (isValid(arr)) {
+			this.weight = arr[0];
+			this.cost = arr[1];
+		}
+	}
+	
+	public long getWeight() {
+		return weight;
+	}
+
+	public long getCost() {
+		return cost;
+	}
+	
+	private boolean isValid(long[] arr) {
+		if (arr.length != 2)
+			return false;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] < 0)
+				return false;
+		}
+		return true;
+	}
+}
